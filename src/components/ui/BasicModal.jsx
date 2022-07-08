1aac79bc781340c3750524fe3cc4d3cc04ebcb19
Halida81 <halidas81@gmail.com>
2022-07-08 19:25:58 +0600

    main modal done

--- src/components/ui/BasicModal.jsx
+++ src/components/ui/BasicModal.jsx
@@ -1,38 +1,30 @@
-import { Modal, Box } from '@mui/material'
-import styled from 'styled-components'
+import { Modal, Box, styled } from '@mui/material'
 
-function BasicModal({ open, onClose, title, children }) {
+function BasicModal({ open, onClose, children }) {
     return (
         <Modal open={open} onClose={onClose}>
-            <Box sx={style}>
-                <StyleHeaderTitle>{title}</StyleHeaderTitle>
+            <StyleBox>
                 <div>{children}</div>
-            </Box>
+            </StyleBox>
         </Modal>
     )
 }
 
 export default BasicModal
 
-const StyleHeaderTitle = styled.h4`
-    width: 195px;
-    height: 32px;
-    font-family: 'Inter';
+const StyleBox = styled(Box)`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 24px 32px;
+    font-family: 'Inter', sans-serif;
     font-style: normal;
     font-weight: 500;
     font-size: 24px;
     line-height: 32px;
     color: #23262f;
+    background-color: white;
+    border-radius: 10px;
+    outline: none;
 `
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    borderRadius: '10px',
-    p: 4,
-}
