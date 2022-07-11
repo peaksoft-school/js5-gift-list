d36aca0c7891aa2176ab3cc4ec1e271c344833a4
doolo11 <doolot928@gmail.com>
2022-07-11 18:51:47 +0600

    radio done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Radio from './components/ui/Radio'
+
 function App() {
-    return <div className="App">gift list</div>
+    return (
+        <div className="App">
+            gift list
+            <Radio />
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/ui/Radio.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+function Radio({ children, ...props }) {
+    return (
+        <Div>
+            <Label>
+                <Input type="radio" name="simple" {...props} />
+                <Span> </Span>
+            </Label>
+            {children}
+        </Div>
+    )
+}
+
+export default Radio
+const Div = styled('div')`
+    display: flex;
+`
+
+const Label = styled.label`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    border: 1px solid #3772ff;
+    display: flex;
+    flex-direction: column;
+    display: inline-block;
+    margin-right: 10px;
+`
+
+const Span = styled.span`
+    display: block;
+    width: 9px;
+    height: 9px;
+    border-radius: 50%;
+    border: 6px solid #3772ff;
+    opacity: 0;
+    margin: -0.7px 0 0 -0.7px;
+`
+
+const Input = styled.input`
+    display: none;
+    &:checked + Span {
+        opacity: 1;
+    }
+`
