38800a5f9f263523a7c743b01c5000ba256a1852
JYLDYZ <jtorogulova3883@gmail.com>
2022-07-12 21:31:35 +0600

    change into dynamic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import BasicMenu from './components/ui/meatBall/components/meatBalls'
+import anonim from './assets/images/анонимно.png'
+import reserve from './assets/images/Забронировать.png'
+import present from './assets/images/подарки.png'
+import dislike from './assets/images/Пожаловаться.png'
+import MeatBalls from './components/ui/meatBall/components/meatBalls'
 
 function App() {
+    const forExample = [
+        {
+            title: 'Забронировать',
+            id: 'e1',
+            icon: anonim,
+        },
+        {
+            title: 'Забронировать анонимно',
+            id: 'e2',
+            icon: reserve,
+        },
+        {
+            title: 'Добавить в мои подарки',
+            id: 'e3',
+            icon: present,
+        },
+        {
+            title: 'Пожаловаться',
+            id: 'e4',
+            icon: dislike,
+        },
+    ]
     return (
         <div className="App">
-            <BasicMenu />
+            <MeatBalls navigations={forExample} />
         </div>
     )
 }
--- src/components/ui/meatBall/components/meatBalls.jsx
+++ src/components/ui/meatBall/components/meatBalls.jsx
@@ -6,12 +6,8 @@ import MenuItem from '@mui/material/MenuItem'
 import styled from 'styled-components'
 
 import balls from '../../../../assets/images/Vector (4).png'
-import anonim from '../../../../assets/images/анонимно.png'
-import reserve from '../../../../assets/images/Забронировать.png'
-import present from '../../../../assets/images/подарки.png'
-import dislike from '../../../../assets/images/Пожаловаться.png'
 
-export default function BasicMenu() {
+export default function MeatBalls(props) {
     const [anchorEl, setAnchorEl] = useState(null)
     const open = Boolean(anchorEl)
 
@@ -42,22 +38,12 @@ export default function BasicMenu() {
                     'aria-labelledby': 'basic-button',
                 }}
             >
-                <MenuItem onClick={handleClose}>
-                    <Img src={reserve} alt="reserve" />
-                    Забронировать
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                    <Img src={anonim} alt="anonimPhtot" />
-                    Забронировать анонимно
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                    <Img src={present} alt="present" />
-                    Добавить в мои подарки
-                </MenuItem>
-                <MenuItem onClick={handleClose}>
-                    <Img src={dislike} alt="dislike" />
-                    Пожаловаться
-                </MenuItem>
+                {props.navigations.map((el) => (
+                    <MenuItem key={el.id}>
+                        <Img src={el.icon} alt="navigations" />
+                        {el.title}
+                    </MenuItem>
+                ))}
             </Menu>
         </div>
     )
