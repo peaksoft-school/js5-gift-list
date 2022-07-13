ee8096fb024fa743a30e9b51151b8c989fe0b5f6
doolo11 <doolot928@gmail.com>
2022-07-13 14:03:17 +0600

    input fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Input from './components/ui/Input'
+import InputPassword from './components/ui/InputPassword'
+
 function App() {
-    return <div className="App">gift list</div>
+    return (
+        <div className="App">
+            gift list
+            <Input />
+            <InputPassword />
+        </div>
+    )
 }
 
 export default App
