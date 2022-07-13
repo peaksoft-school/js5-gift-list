99bdda427109546383894f3147ab3cfd437c5d9f
doolo11 <doolot928@gmail.com>
2022-07-13 18:02:21 +0600

    fixed again

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-import { useState } from 'react'
-
-import Radio from './components/ui/Radio'
-
 function App() {
-    const [cheked, setCheked] = useState(false)
-    const a = 'text'
-    console.log(cheked)
-    const f = (e) => {
-        setCheked(e.target.checked)
-    }
-    return (
-        <div className="App">
-            gift list
-            <Radio onChange={f} cheked={cheked} label={a} />
-        </div>
-    )
+    return <div className="App">gift list</div>
 }
 
 export default App
