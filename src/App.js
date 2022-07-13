ddeed7c28d46c5c1740281be068ad2f3995fdb0b
JYLDYZ <jtorogulova3883@gmail.com>
2022-07-13 22:18:21 +0600

    unfinished

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import anonim from './assets/images/анонимно.png'
-import reserve from './assets/images/Забронировать.png'
-import present from './assets/images/подарки.png'
-import dislike from './assets/images/Пожаловаться.png'
 import MeatBalls from './components/ui/meatBall/components/meatBalls'
 
 function App() {
-    const forExample = [
-        {
-            title: 'Забронировать',
-            id: 'e1',
-            icon: anonim,
-        },
-        {
-            title: 'Забронировать анонимно',
-            id: 'e2',
-            icon: reserve,
-        },
-        {
-            title: 'Добавить в мои подарки',
-            id: 'e3',
-            icon: present,
-        },
-        {
-            title: 'Пожаловаться',
-            id: 'e4',
-            icon: dislike,
-        },
-    ]
     return (
         <div className="App">
-            <MeatBalls navigations={forExample} />
+            <MeatBalls />
         </div>
     )
 }
