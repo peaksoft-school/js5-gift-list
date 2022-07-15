fca730b64b79a49316e0bf13b850d7159db9fe33
Halida81 <halidas81@gmail.com>
2022-07-15 19:26:16 +0600

    search input no filter

--- src/components/ui/MainSearchInput.jsx
+++ src/components/ui/MainSearchInput.jsx
@@ -1,22 +1,18 @@
 import SearchIcon from '@mui/icons-material/Search'
-import { InputBase, IconButton, styled, Avatar } from '@mui/material'
+import { InputBase, IconButton, Paper, styled, Avatar } from '@mui/material'
 
 export default function MainSearchInput({
-    USERS,
+    options,
     onChange,
     open,
     value,
     onClick,
 }) {
-    const filteredUser = USERS.filter((user) => {
-        return user.name.toLowerCase().includes(value.toLowerCase())
-    })
-
     const renderSearchResults = () => {
-        if (!filteredUser) {
+        if (!options) {
             return <p>Такого пользователя не существует</p>
         }
-        return filteredUser.map((user) => {
+        return options.map((user) => {
             return (
                 <StyledUserDiv key={user.id}>
                     <StyledAvatar alt="Remy Sharp" src={user.img} />
@@ -28,7 +24,7 @@ export default function MainSearchInput({
 
     return (
         <>
-            <StyledForm>
+            <StyledFormPaper>
                 <IconButton>
                     <MuiSearchIcon />
                 </IconButton>
@@ -38,31 +34,30 @@ export default function MainSearchInput({
                     onChange={onChange}
                     onClick={onClick}
                 />
-            </StyledForm>
-            <ResultContainer>
-                {value.trim().length >= 1 && open && (
-                    <ResultDiv>
-                        <StyledTitle>Результаты поиска</StyledTitle>
-                        <hr />
-                        <StyledContentTitle>
-                            {renderSearchResults()}
-                        </StyledContentTitle>
-                    </ResultDiv>
-                )}
-            </ResultContainer>
+            </StyledFormPaper>
+            {value.trim().length >= 1 && open && (
+                <ResultDiv>
+                    <StyledTitle>Результаты поиска</StyledTitle>
+                    <hr />
+                    <StyledContentTitle>
+                        {renderSearchResults()}
+                    </StyledContentTitle>
+                </ResultDiv>
+            )}
         </>
     )
 }
 
 const ResultDiv = styled('div')`
-    background-color: #f4f6f6;
+    background-color: #eef2f2;
     border-radius: 8px;
     position: absolute;
+    height: auto;
     top: 65px;
     width: 823px;
 `
 
-const StyledForm = styled('form')`
+const StyledFormPaper = styled(Paper)`
     box-sizing: border-box;
     display: flex;
     justify-content: flex-end;
@@ -74,14 +69,16 @@ const StyledForm = styled('form')`
     border: 1px solid #bdbdbd;
     border-radius: 8px;
     &:hover {
-        border: 1px solid #8639b5;
+        border: 2px solid #8639b5;
+    }
+    &:focus-within {
+        border: 2px solid #8639b5;
     }
 `
 
 const MuiInputBase = styled(InputBase)`
-    width: 743px;
+    width: 721px;
     height: 17px;
-    margin: -20px;
     font-style: normal;
     font-weight: 400;
     font-size: 14px;
@@ -96,29 +93,17 @@ const MuiInputBase = styled(InputBase)`
 const MuiSearchIcon = styled(SearchIcon)`
     width: 20px;
     height: 20px;
-    padding: 10px;
 `
 
-const ResultContainer = styled('div')`
-    height: 100%;
-    width: 821px;
-    background-color: white;
-    border-radius: 3%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`
 const StyledTitle = styled('p')`
     font-style: normal;
     font-weight: 500;
-    margin: 0;
     font-size: 18px;
-    padding: 5px;
     line-height: 22px;
-    display: flex;
-    align-items: center;
+    height: 22px;
     letter-spacing: 0.2px;
     color: #020202;
+    margin-left: 18px;
 `
 
 const StyledSpan = styled('span')`
@@ -127,25 +112,23 @@ const StyledSpan = styled('span')`
 
 const StyledUserDiv = styled('div')`
     display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin: 16px;
 `
 
 const StyledContentTitle = styled('span')`
-    width: 374px;
-    height: 22px;
-    padding: 5px;
+    height: 19px;
     font-family: inter;
     font-style: normal;
     font-weight: 500;
-    font-size: 18px;
-    line-height: 22px;
-    display: flex;
-    align-items: center;
+    font-size: 16px;
+    line-height: 19px;
     letter-spacing: 0.2px;
     color: #020202;
 `
 
 const StyledAvatar = styled(Avatar)`
-    width: 35px;
-    height: 35px;
-    margin-bottom: 9px;
+    width: 40px;
+    height: 40px;
 `
