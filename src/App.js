ad68bed79427c7dcba0e31ef745899d90df71e47
Bilal <b1335190@gmail.com>
2022-07-18 17:42:27 +0600

    'test'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import BasicModal from './components/ui/BasicModal'
+
 function App() {
-    return <div className="App">gift list</div>
+    return (
+        <div className="App">
+            gift list
+            <BasicModal>bilal</BasicModal>
+        </div>
+    )
 }
 
 export default App
