c8819881b12f56f583155ee78d0a8c451857fb49
JYLDYZ <jtorogulova3883@gmail.com>
2022-08-19 15:19:40 +0600

    just

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Get from './components/users/Get'
 import MainRouter from './routes/MainRouter'
 
 function App() {
     return (
         <div className="App">
             <MainRouter />
+            <Get />
         </div>
     )
 }
--- src/components/users/Get.jsx
+++ src/components/users/Get.jsx
@@ -1,4 +1,17 @@
+import { appFetch } from '../../api/CustomFetch'
+
 const Get = () => {
+    const datas = {
+        name: 'string',
+        photo: 'string',
+        categoryId: 0,
+        subCategoryId: 0,
+        status: 'USED',
+        description: 'string',
+    }
+    appFetch({ url: '/api/gifts', method: 'POST', body: JSON.stringify(datas) })
+
+    // console.log(data)
     return <div> </div>
 }
 export default Get
